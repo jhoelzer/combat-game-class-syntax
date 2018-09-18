059f009710d19d2b94f1c6a5bce9031e6f19af46
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-09-18 09:26:31 -0400

    changed this.fight to class method

--- index.js
+++ index.js
@@ -6,46 +6,45 @@ class Creature {
         this.chanceToCrit = options.chanceToCrit || 0.2;
         this.chanceToMiss = options.chanceToMiss || 0.3;
         this.baseDamage = options.baseDamage || 25;
-
-        this.fight = function (creature) {
-            let message;
-            // displays the message on the page
-            function actionTaken() {
-                let elementAction = document.createElement("div");
-                let resultAction = document.createTextNode(message);
-                elementAction.appendChild(resultAction);
-                let placeAction = document.getElementById("action");
-                placeAction.appendChild(elementAction);
-            }
-            // decides whether the Creature's attack missed
-            if (Math.random() < this.chanceToMiss) {
-                message = `${this.name} missed ${creature.name}`
-                 // action
-                 actionTaken();
-            } else {
-                // decides whether the attack is a critical hit; sets the amount of damage a critical hit does
-                const dmg = Math.random() < this.chanceToCrit
-                    ? this.baseDamage * 2
-                    : this.baseDamage;
-                // subtracts the amount of damage from the creature's health
-                creature.health -= dmg;
-                // message displayed if the hunters are the attackers
-                if (creature === hunters) {
-                    message = `${creature.name} have been hit! They are now at ${creature.health} health`;
-                    // action
-                    actionTaken();
-                // message displayed if the werewolf is the attacker
-                } else if (creature === werewolf) {
-                message = `${creature.name} has been hit! It's now at ${creature.health} health`;
-                    // action
-                    actionTaken();
-                }
-            }
-            console.log(message);
-            return message;
-        }
     }
-    
+
+    fight(creature) {
+        let message;
+        // displays the message on the page
+        function actionTaken() {
+            let elementAction = document.createElement("div");
+            let resultAction = document.createTextNode(message);
+            elementAction.appendChild(resultAction);
+            let placeAction = document.getElementById("action");
+            placeAction.appendChild(elementAction);
+        }
+        // decides whether the Creature's attack missed
+        if (Math.random() < this.chanceToMiss) {
+            message = `${this.name} missed ${creature.name}`
+                // action
+                actionTaken();
+        } else {
+            // decides whether the attack is a critical hit; sets the amount of damage a critical hit does
+            const dmg = Math.random() < this.chanceToCrit
+                ? this.baseDamage * 2
+                : this.baseDamage;
+            // subtracts the amount of damage from the creature's health
+            creature.health -= dmg;
+            // message displayed if the hunters are the attackers
+            if (creature === hunters) {
+                message = `${creature.name} have been hit! They are now at ${creature.health} health`;
+                // action
+                actionTaken();
+            // message displayed if the werewolf is the attacker
+            } else if (creature === werewolf) {
+            message = `${creature.name} has been hit! It's now at ${creature.health} health`;
+                // action
+                actionTaken();
+            }
+        }
+        console.log(message);
+        return message;
+    }
 }
 
 // Class Inheritance
@@ -56,6 +55,7 @@ class Monster extends Creature {
     }
 }
 
+// Creatures
 // heroes
 const hunters = new Creature ({
     name: "Sam and Dean",
@@ -73,11 +73,11 @@ const werewolf = new Monster ({
 let bulletChoice = prompt("Which bullet: iron or silver?").toLowerCase()
 if (bulletChoice === "iron") {
     // iron does no damage to a werewolf; hunter base damage set to 0
-    // hunters always lose
+    // hunters always lose, but still attack since user chose a valid weapon
     hunters.baseDamage = 0;
 } else if (bulletChoice === "silver") {
     // silver is effective against a werewolf; hunter base damage set to 25
-    // hunters won't always win; depends on luck
+    // hunters won't always win; depends on luck (misses and critical hits)
     hunters.baseDamage = 25;
 } else if (bulletChoice === "colt") {
     // the Colt can kill almost anything with one shot; hunter base damage set to 100;
@@ -94,7 +94,6 @@ if (bulletChoice === "iron") {
     hunters.baseDamage = 0;
     hunters.health = 0;
 }
-
 
 // unique message about the user's chosen weapon
 let weaponChoice;
@@ -122,7 +121,7 @@ function battle (hero, ...monsters) {
     }
     
     // keeps fight going as long as both the heroes and monster have more than 0 health
-    // at least it's fucking supposed to...
+    // appears to run both hero.fight(monster) and monster.fight(hero) at same time, leading to both attacking simultaneously, leading to cases where a character at 0 health still attacks
     monsters.forEach(monster => {
         while (hero.health > 0 && monster.health > 0) {
             hero.fight(monster);
@@ -161,6 +160,7 @@ function battle (hero, ...monsters) {
 // run the battle code
 battle(hunters, werewolf);
 
+// reloads page
 function replay() {
     location.reload();
 }
